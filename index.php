da8f9574c3ce8622941ff3998cbec47b2de1368f
matthewcumbo <matthew.abraham.cumbo@mcast.edu.mt>
2025-10-29 12:44:16 +0100

    Added comments

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 
 <?php 
 /*
-    The include function can be used to include the contents of 
-    other files, called templates or partial pages
+    The include statement can be used to include the contents of 
+    other files, called templates or partial pages.
+    This can be used to split up our pages into templates
+    to make development of the web application more efficient.
 */
 include "header.php"; ?> 
 
@@ -13,17 +15,20 @@ include "header.php"; ?>
     -->
 
     <?php 
-        // The echo function is used to create output into the page
+        // The echo statement is used to create output into the page
         echo "<p>Hello from PHP!</p>";
         echo "<p>Second output</p>";
 
-        // Variables are created similar to JS, but they always start with a $
+        /* 
+            Variables are created similar to JS, but they always start with a $
+            They are dynamic; i.e. their type inferred by the given value.
+            */
         $name = "Joe";
         $surname = "Borg";
 
         /* 
-            The below is an example of concatenation, where 
-            we can join multiple strings together
+            The below is an example of concatenation, where we can join multiple strings together.
+            We can use the echo statement to output the value of a variable
         */
         echo "<h2>".$name." ".$surname."</h2>";
 
---
+++ strings.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+
+<div class="border border-dark m-3 p-3">
+<h1>String Functions</h1>
+<?php
+    $fullName = "Matthew Cumbo";
+
+    /* 
+        The strlen function returns the length of a string. Here we are placing that result into a new variable.
+    */
+    $fullNameLength = strlen($fullName);
+    /*
+        The reuslt of a function can be concatenated within an echo statement, or other functions. 
+    */ 
+    echo "<p>My full name {$fullName} is made up of {$fullNameLength} character.</p>";
+
+    /*
+        The substr function extracts a SubString out of an existing string, starting at the position provided as a parameter, and the length of the substring is the final parameter provided
+    */
+    $firstName = substr($fullName, 0, 7);
+    echo "<p>First Name: {$firstName}</p>";
+
+    /*
+        We can use the substr function without providing the final parameter means that it will extract content from the given position till the end of the string
+    */
+    $lastName = substr($fullName,8);
+    echo "<p>Last Name: {$lastName}</p>";
+?>
+</div>
+
+<div class="border border-dark m-3 p-3">
+    <h1>Advanced Functions</h1>
+    <?php 
+        $shoppingList = "bread,milk,apples";
+        /* 
+            The explode function turns a string into an array, using a character within the string as a separator
+        */
+        $shoppingListArray = explode(",",$shoppingList);
+
+        /*
+            The prinr_r function simply outputs the array, including its values and positions 
+        */
+        print_r($shoppingListArray);
+
+        /* 
+            The implode functions builds a string out of an array, adding a separator between each value
+        */
+        $outputList = implode(", ",$shoppingListArray);
+        echo "<p>{$outputList}</p>";
+    ?>
+</div>
+
+
+<?php include "footer.php" ?>
